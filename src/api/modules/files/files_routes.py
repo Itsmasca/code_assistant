d1bcbf45ea35653f8bd0bffbd645e8a9898005d8
BrenDev0 <lahey1991@gmail.com>
2025-07-22 12:39:53 -0600

    removed unused imports

--- src/api/modules/files/files_routes.py
+++ src/api/modules/files/files_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Body, Request
 from fastapi.responses import JSONResponse
 from core.dependencies.container import Container
 from src.api.core.services.embedding_service import EmbeddingService
-from modules.files.files_models import UploadRequest
+
 
 router = APIRouter(
     prefix="/api/files",
@@ -10,31 +10,3 @@ router = APIRouter(
 )
 
 
-@router.post("/upload", response_class=JSONResponse)
-async def upload_docs(
-    request: Request,
-    data: UploadRequest = Body(...)
-):
-    try:
-        user_id = request.state.user_id
-        s3_url = data.s3_url
-        file_type = data.file_type
-        filename = data.filename
-        agent_id = data.agent_id
-
-
-        embedding_service: EmbeddingService = Container.resolve("embedding_service") 
-        status = await embedding_service.embed_uploaded_document(
-            s3_url = s3_url,
-            file_type=file_type,
-            filename=filename,
-            user_id=user_id,
-            agent_id=agent_id,
-        )
-
-        print(status)
-
-        return JSONResponse(status_code=200, content={"message": "Document added to vector store"});
-
-    except Exception as e:
-        print(e)
