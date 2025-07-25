a28406cf3fe87a2353d15714e16727d6db278194
BrenDev0 <lahey1991@gmail.com>
2025-07-25 12:10:24 -0600

    init not called bug

--- src/api/modules/chats/messages/message_dependencies.py
+++ src/api/modules/chats/messages/message_dependencies.py
@@ -6,7 +6,7 @@ from src.api.modules.chats.messages.messages_controller import MessagesControlle
 from src.api.core.services.http_service import HttpService
 
 def configure_messages_dependencies(logger: Logger, http_service: HttpService):
-    repository = MessagesRepository
+    repository = MessagesRepository()
     service = MessagesService(logger=logger, repository=repository)
     controller = MessagesController(https_service=http_service, messages_service=service)
 
